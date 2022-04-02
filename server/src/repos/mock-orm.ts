cac877be167295290c61f739e44ae82b457088b4
Geoff Whitehead <geoff1012@gmail.com>
2022-04-02 17:23:13 +0100

    comment out code

--- server/src/repos/mock-orm.ts
+++ server/src/repos/mock-orm.ts
@@ -1,32 +1,29 @@
-import jsonfile from 'jsonfile';
+// import { IUser } from "@models/user-model";
+// import jsonfile from "jsonfile";
 
+// const dbFilePath = "src/repos/database.json";
 
-const dbFilePath = 'src/repos/database.json';
+// /**
+//  * Fetch the json from the file.
+//  *
+//  * @returns
+//  */
+// function openDb(): Promise<Record<string, { users: IUser[] }>> {
+//   return jsonfile.readFile(dbFilePath);
+// }
 
+// /**
+//  * Update the file.
+//  *
+//  * @param db
+//  * @returns
+//  */
+// function saveDb(db: Record<string, any>): Promise<void> {
+//   return jsonfile.writeFile(dbFilePath, db);
+// }
 
-/**
- * Fetch the json from the file.
- * 
- * @returns 
- */
-function openDb(): Promise<Record<string, any>> {
-    return jsonfile.readFile(dbFilePath);
-}
-
-
-/**
- * Update the file.
- * 
- * @param db 
- * @returns 
- */
-function saveDb(db: Record<string, any>): Promise<void> {
-    return jsonfile.writeFile(dbFilePath, db);
-}
-
-
-// Export default
-export default {
-    openDb,
-    saveDb,
-} as const;
+// // Export default
+// export default {
+//   openDb,
+//   saveDb,
+// } as const;
